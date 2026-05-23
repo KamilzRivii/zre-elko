eb9943c4f50d54a436bd792efc251c1c5a38a406
KamilzRivii <kamilprzybyla2@gmail.com>
2026-05-23 22:29:52 +0200

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,13 @@
+import dynamic from "next/dynamic"
 import { HeroSection } from "@/components/sections/home/HeroSection"
 import { AboutHeroSection } from "@/components/sections/o-nas/AboutHeroSection"
 import { CertyfikatySection } from "@/components/sections/certyfikaty/CertyfikatySection"
 import { RealizacjeCarousel } from "@/components/sections/realizacje/RealizacjeCarousel"
 import { OfertaDescSection } from "@/components/sections/home/OfertaDescSection"
-import { ContactSection } from "@/components/sections/kontakt/ContactSection"
+
+const ContactSection = dynamic(() =>
+  import("@/components/sections/kontakt/ContactSection").then((m) => m.ContactSection)
+)
 
 export default function HomePage() {
   return (
